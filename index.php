5c04f06a0eb4561099154aeed0ae90c72d04795f
faridreaming <97384053+faridreaming@users.noreply.github.com>
2024-11-11 20:31:30 +0700

    chore: add a starter file for php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="./dist/output.css" />
+    <link
+      rel="shortcut icon"
+      href="./dist/assets/logo-white.svg"
+      type="image/x-icon"
+    />
+  </head>
+
+  <body class="h-svh">
+    <script src="./dist/js/script.js"></script>
+  </body>
+</html>
