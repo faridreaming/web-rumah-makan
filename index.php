bccccf828c960110cb574c065a9df309eb179239
faridreaming <97384053+faridreaming@users.noreply.github.com>
2024-11-14 11:04:17 +0700

    feat: add login and register page

--- index.php
+++ index.php
@@ -4,15 +4,157 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
+
     <link rel="stylesheet" href="./dist/output.css" />
     <link
       rel="shortcut icon"
       href="./dist/assets/logo-white.svg"
       type="image/x-icon"
     />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+      rel="stylesheet"
+    />
   </head>
 
-  <body class="h-svh">
+  <body class="flex h-svh items-center justify-center bg-gray-100">
+    <div class="container w-fit overflow-hidden rounded-xl bg-white shadow-xl">
+      <div
+        id="login_show"
+        class="group relative flex flex-col gap-2 overflow-hidden p-10"
+      >
+        <div
+          class="absolute bottom-0 left-0 flex h-full w-full flex-col items-center justify-center gap-2 p-10 text-center text-white transition-all"
+        >
+          <div
+            class="absolute bottom-0 left-0 h-full w-full bg-gradient-to-b from-lime-500 to-green-500 transition-all group-target:-bottom-full"
+          ></div>
+          <div
+            class="absolute z-10 flex flex-col items-center justify-center gap-2 p-10 text-center transition-[transform,opacity] duration-[500ms,300ms] group-target:-translate-y-10 group-target:opacity-0"
+          >
+            <h1 class="heading-1">Welcome To Our Web!</h1>
+            <p>Silahkan login jika sudah punya akun.</p>
+            <a
+              href="./#login_show"
+              class="mt-2 w-1/2 rounded-full border-2 border-white py-2 font-semibold text-white outline-none transition-all duration-[250ms] hover:border-green-500 hover:bg-white hover:text-green-500 focus:border-green-500 focus:bg-white focus:text-green-500"
+            >
+              Login
+            </a>
+          </div>
+        </div>
+        <div
+          class="translate-y-10 opacity-0 transition-[transform,opacity] duration-[500ms,300ms] group-target:z-20 group-target:translate-y-0 group-target:opacity-100 group-target:duration-[500ms,500ms]"
+        >
+          <h1 class="heading-1 text-center">Login</h1>
+          <form action="./login.php" method="post" class="flex flex-col gap-2">
+            <div class="flex flex-col gap-1">
+              <label for="username">Username</label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                class="rounded border-2 border-gray-400 p-1 text-gray-400 outline-none transition-all duration-[250ms] focus:border-gray-800 focus:text-gray-800"
+                spellcheck="false"
+              />
+            </div>
+            <div class="flex flex-col gap-1">
+              <label for="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                class="rounded border-2 border-gray-400 p-1 text-gray-400 outline-none transition-all duration-[250ms] focus:border-gray-800 focus:text-gray-800"
+                spellcheck="false"
+              />
+            </div>
+            <button
+              type="submit"
+              name="login"
+              id="login"
+              class="mx-auto mt-2 w-1/2 rounded-full bg-lime-500 py-2 font-semibold text-white outline-none transition-all duration-[250ms] hover:bg-lime-600 focus:bg-lime-600"
+            >
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <div
+        id="register_show"
+        class="group relative flex flex-col gap-2 overflow-hidden p-10"
+      >
+        <div
+          class="absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center gap-2 p-10 text-center text-white transition-all"
+        >
+          <div
+            class="absolute left-0 top-0 h-full w-full bg-gradient-to-b from-lime-500 to-green-500 transition-all group-target:-top-full"
+          ></div>
+          <div
+            class="absolute z-10 flex flex-col items-center justify-center gap-2 p-10 text-center transition-[transform,opacity] duration-[500ms,300ms] group-target:-translate-y-10 group-target:opacity-0"
+          >
+            <h1 class="heading-1">Welcome To Our Web!</h1>
+            <p>Silahkan registrasi akun untuk login.</p>
+            <a
+              href="./#register_show"
+              class="mt-2 w-1/2 rounded-full border-2 border-white py-2 font-semibold text-white outline-none transition-all duration-[250ms] hover:border-green-500 hover:bg-white hover:text-green-500 focus:border-green-500 focus:bg-white focus:text-green-500"
+            >
+              Register
+            </a>
+          </div>
+        </div>
+        <div
+          class="-translate-y-10 opacity-0 transition-[transform,opacity] duration-[500ms,300ms] group-target:z-20 group-target:translate-y-0 group-target:opacity-100 group-target:duration-[500ms,500ms]"
+        >
+          <h1 class="heading-1 text-center">Register</h1>
+          <form
+            action="./register.php"
+            method="post"
+            class="flex flex-col gap-2"
+          >
+            <div class="flex flex-col gap-1">
+              <label for="register_username">Username</label>
+              <input
+                type="text"
+                name="register_username"
+                id="register_username"
+                class="rounded border-2 border-gray-400 p-1 text-gray-400 outline-none transition-all duration-[250ms] focus:border-gray-800 focus:text-gray-800"
+                spellcheck="false"
+              />
+            </div>
+            <div class="flex flex-col gap-1">
+              <label for="register_password">Password</label>
+              <input
+                type="password"
+                name="register_password"
+                id="register_password"
+                class="rounded border-2 border-gray-400 p-1 text-gray-400 outline-none transition-all duration-[250ms] focus:border-gray-800 focus:text-gray-800"
+                spellcheck="false"
+              />
+            </div>
+            <div class="flex flex-col gap-1">
+              <label for="confirm_password">Confirm Password</label>
+              <input
+                type="password"
+                name="confirm_password"
+                id="confirm_password"
+                class="rounded border-2 border-gray-400 p-1 text-gray-400 outline-none transition-all duration-[250ms] focus:border-gray-800 focus:text-gray-800"
+                spellcheck="false"
+              />
+            </div>
+            <button
+              type="submit"
+              name="register"
+              id="register"
+              class="mx-auto mt-2 w-1/2 rounded-full bg-green-500 py-2 font-semibold text-white outline-none transition-all duration-[250ms] hover:bg-green-600 focus:bg-green-600"
+            >
+              Register
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
     <script src="./dist/js/script.js"></script>
   </body>
 </html>
