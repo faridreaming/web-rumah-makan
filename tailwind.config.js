8bb8a98b85fecbad3c116a9840ff90bb82e851c0
faridreaming <97384053+faridreaming@users.noreply.github.com>
2024-11-24 23:24:20 +0700

    feat: remove default properties for transition

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,6 @@
 module.exports = {
   content: ["./*.php"],
   theme: {
-    transitionDuration: {
-      DEFAULT: "500ms",
-    },
-    transitionTimingFunction: {
-      DEFAULT: "ease-in-out",
-    },
     extend: {
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
