5c46634c9200c2c53093d5d41221409d221587b0
faridreaming <97384053+faridreaming@users.noreply.github.com>
2024-11-29 10:51:32 +0700

    feat: add toast

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,4 @@
+// Navbar
 const navToggle = document.getElementById("nav-toggle");
 const navbar = document.getElementById("navbar");
 
@@ -16,3 +17,17 @@ navToggle.addEventListener("click", () => {
   const isNavOpen = document.body.classList.contains("nav-open");
   localStorage.setItem("navOpen", isNavOpen);
 });
+
+// Toast
+const toast = document.getElementById("toast-message");
+if (toast) {
+  setTimeout(() => {
+    toast.style.bottom = "20px";
+  }, 100);
+  setTimeout(() => {
+    toast.style.bottom = "-100px";
+  }, 3000);
+  setTimeout(() => {
+    toast.remove();
+  }, 3500);
+}
