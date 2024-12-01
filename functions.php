c1eddfbc8571d06d742a498feff208064aa161e3
faridreaming <97384053+faridreaming@users.noreply.github.com>
2024-12-01 11:59:34 +0700

    feat: add query function

--- functions.php
+++ functions.php
@@ -6,3 +6,15 @@ $conn = mysqli_connect("localhost", "root", "", "rm_padang");
 if (!$conn) {
   die("Koneksi gagal: " . mysqli_connect_error());
 }
+
+// Query data
+function query($query)
+{
+  global $conn;
+  $result = mysqli_query($conn, $query);
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)):
+    $rows[] = $row;
+  endwhile;
+  return $rows;
+}
