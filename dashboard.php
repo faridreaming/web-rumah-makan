2a3b392eac212212bb93095b93b3cedb641819cc
faridreaming <97384053+faridreaming@users.noreply.github.com>
2024-12-01 11:58:46 +0700

    feat: add back button

--- dashboard.php
+++ dashboard.php
@@ -129,15 +129,30 @@ require_once('./components/head.php');
       function showBreadCrumb($parent, $children)
       {
         $childrenText = implode(' &rsaquo; ', array_map('ucwords', $children));
-        echo '
-          <div class="flex p-2 sm:p-4 border-b-2 border-gray-200 text-xs sm:text-sm">
-            <h3 class="font-medium">
+        if (isset($_GET["action"])) {
+          echo '
+            <div class="flex p-2 sm:p-4 border-b-2 border-gray-200 text-xs sm:text-sm justify-between items-center">
+              <h3 class="font-medium">
               ' . ucwords($parent) . ' <span class="text-gray-500">&rsaquo; ' . $childrenText . '</span>
-            </h3>
-          </div>
-        ';
+              </h3>
+              <a href="?page=' . $children[0] . '" class="h-min flex items-center justify-center text-gray-600 hover:text-gray-400">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF" class="fill-current transition duration-300 ease-in-out"><path d="M280-200v-80h284q63 0 109.5-40T720-420q0-60-46.5-100T564-560H312l104 104-56 56-200-200 200-200 56 56-104 104h252q97 0 166.5 63T800-420q0 94-69.5 157T564-200H280Z"/></svg>
+              </a>
+            </div>
+          ';
+        } else {
+          echo '
+            <div class="flex p-2 sm:p-4 border-b-2 border-gray-200 text-xs sm:text-sm">
+              <h3 class="font-medium">
+              ' . ucwords($parent) . ' <span class="text-gray-500">&rsaquo; ' . $childrenText . '</span>
+              </h3>
+            </div>
+          ';
+        }
       }
+      ?>
 
+      <?php
       switch ($page) {
         case 'dashboard':
           showBreadCrumb("dashboard", [$page]);
