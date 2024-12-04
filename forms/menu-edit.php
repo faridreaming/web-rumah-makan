ec3476dc7cf24a883ad59e915bdd0d0bc7fc1816
faridreaming <97384053+faridreaming@users.noreply.github.com>
2024-12-04 08:06:18 +0700

    feat: fix ui

--- forms/menu-edit.php
+++ forms/menu-edit.php
@@ -52,12 +52,12 @@ $menu = query("SELECT * FROM menu WHERE id_menu = $id")[0];
 <?php
 if (isset($_SESSION["success"])) {
   echo '<div id="toast-message" 
-    class="bg-gray-800 rounded p-2 fixed right-1/2 translate-x-1/2 bottom-[-100px] text-white font-medium w-fit transition-all duration-500 ease-in-out">'
+    class="bg-gray-800 rounded p-2 fixed right-1/2 translate-x-1/2 bottom-[-100px] text-white font-medium w-fit transition-all duration-500 ease-in-out text-center">'
     . $_SESSION["success"] . '</div>';
   unset($_SESSION["success"]);
 } else if (isset($_SESSION["error"])) {
   echo '<div id="toast-message" 
-    class="bg-red-500 rounded p-2 fixed right-1/2 translate-x-1/2 bottom-[-100px] text-white font-medium w-fit transition-all duration-500 ease-in-out">'
+    class="bg-red-500 rounded p-2 fixed right-1/2 translate-x-1/2 bottom-[-100px] text-white font-medium w-fit transition-all duration-500 ease-in-out text-center">'
     . $_SESSION["error"] . '</div>';
   unset($_SESSION["error"]);
 }
