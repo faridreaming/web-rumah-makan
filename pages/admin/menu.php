66a409fa2541a95459ab648505149baee3dba43c
faridreaming <97384053+faridreaming@users.noreply.github.com>
2024-12-04 08:05:33 +0700

    feat: fix ui

--- pages/admin/menu.php
+++ pages/admin/menu.php
@@ -74,7 +74,7 @@ $totalMenu = count($menus);
               <img src="<?= $src ?>" alt="<?= $menu['nama_menu'] ?>" class="w-10 h-10 aspect-square object-cover rounded-full">
               <?= $menu["nama_menu"] ?>
             </td>
-            <td><?= "Rp " . number_format($menu["harga_menu"], 0, ",", ".") ?></td>
+            <td class="whitespace-nowrap"><?= "Rp " . number_format($menu["harga_menu"], 0, ",", ".") ?></td>
             <td><?= $menu["jenis_menu"] ?></td>
             <td><?= $menu["stok"] ?></td>
             <td>
@@ -103,12 +103,12 @@ $totalMenu = count($menus);
 <?php
 if (isset($_SESSION["success"])) {
   echo '<div id="toast-message" 
-    class="bg-gray-800 rounded p-2 fixed right-1/2 translate-x-1/2 bottom-[-100px] text-white font-medium w-fit transition-all duration-500 ease-in-out">'
+    class="bg-gray-800 rounded p-2 fixed right-1/2 translate-x-1/2 bottom-[-100px] text-white font-medium w-fit transition-all duration-500 ease-in-out text-center">'
     . $_SESSION["success"] . '</div>';
   unset($_SESSION["success"]);
 } else if (isset($_SESSION["error"])) {
   echo '<div id="toast-message" 
-    class="bg-red-500 rounded p-2 fixed right-1/2 translate-x-1/2 bottom-[-100px] text-white font-medium w-fit transition-all duration-500 ease-in-out">'
+    class="bg-red-500 rounded p-2 fixed right-1/2 translate-x-1/2 bottom-[-100px] text-white font-medium w-fit transition-all duration-500 ease-in-out text-center">'
     . $_SESSION["error"] . '</div>';
   unset($_SESSION["error"]);
 }
